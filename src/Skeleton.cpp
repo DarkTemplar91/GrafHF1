ab569c4eb69b87abb7401c9c8b134a66c5800471
DarkTemplar91 <50337392+DarkTemplar91@users.noreply.github.com>
2022-04-14 19:42:53 +0200

    Physics left

--- src/Skeleton.cpp
+++ src/Skeleton.cpp
@@ -57,43 +57,53 @@ private:
     Atom* parent;
     double mass;
     double charge;
-    unsigned int cX;
-    unsigned int cY;
+    double cX;
+    double cY;
+    double vX=0;
+    double vY=0;
 
 public:
     Atom(){
         mass=0.1;
         charge=0.1;
-        cX=0;
-        cY=0;
+        cX=-1;
+        cY=-1;
         parent= nullptr;
     }
     Atom(Atom* parent){
-        mass=(rand()%100+1)*(pow(1.6735575,-24) );
-        charge=(rand()%200-100)*(pow(1.60217662, -19));
+        mass=rand()%300+1;
+        charge=(rand()%200-100);
         this->parent=parent;
-        do{
-            cX=((rand() %600 +1));
-            cY=(rand()%600+1);
 
-        }while(!isInsideRadius(cX,cY,100));
+
+
+        do{
+            cX=-1.0f + (double)rand()/ RAND_MAX * (1.f - -1.f);
+            cY=-1.0f + (double)rand()/ RAND_MAX * (1.f - -1.f);
+
+        }while(!isInsideRadius(0.5));
 
     }
-    bool isInsideRadius(unsigned int x, unsigned int y, unsigned int radius){
+    bool isInsideRadius(double radius){
         if(parent==nullptr)
             return true;
-        if((x-parent->cX)*(x-parent->cX)+(y-parent->cY)*(y-parent->cY)<=radius*radius)
+        if((cX-parent->cX)*(cX-parent->cX)+(cY-parent->cY)*(cY-parent->cY)<=radius*radius)
             return true;
         return false;
 
     }
-    unsigned int getX() {return cX;}
-    unsigned int getY() {return cY;}
+    double getX() {return cX;}
+    double getY() {return cY;}
     double getCharge(){return charge;}
     double getMass(){return mass;}
     void setCharge(double value){charge=value;}
-
-    void draw(unsigned int r, unsigned int res){
+    void setX(double val){
+        cX=val;
+    }
+    void setY(double val){
+        cY=val;
+    }
+    void draw(double r, unsigned int res){
         GLfloat doublePi=2*M_PI;
 
         GLfloat circleVerticesX[res];
@@ -103,13 +113,13 @@ public:
             circleVerticesX[i]=cX+r* cos(i*doublePi/res);
             circleVerticesY[i]=cY+r*sin(i*doublePi/res);
         }
-        Vertex* vertices=new Vertex[res];
+        Vertex vertices[res];
         for(int i=0;i<res;i++){
-            vertices[i]=Vertex(vec2(2*circleVerticesX[i]/600-1,2*circleVerticesY[i]/600-1),(charge<0 ? vec3(0,0,pow(1.60217662, -19)*100/(-1*charge)): vec3(pow(1.60217662, -19)*100/charge,0,0)));
+            vertices[i]=Vertex(vec2(circleVerticesX[i],circleVerticesY[i]),(charge<0 ? vec3(0,0,100/(-1*charge)): vec3(100/charge,0,0)));
         }
         glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * res, vertices, GL_STATIC_DRAW);
         glDrawArrays(GL_TRIANGLE_FAN, 0,res);
-        delete[] vertices;
+
 
 
     }
@@ -123,18 +133,26 @@ public:
         double intervalY=(float)diffY/res;
         Vertex vertices[res+1];
         for(int i=0;i<=res;i++){
-            vertices[i]=Vertex(vec2(2*(cX+intervalX*i)/600-1,2*(cY+intervalY*i)/600-1),vec3(1,1,1));
+            vertices[i]=Vertex(vec2(cX+intervalX*i,cY+intervalY*i),vec3(1,1,1));
         }
         glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * res, vertices, GL_STATIC_DRAW);
         glDrawArrays(GL_LINE_STRIP, 0,res);
 
     }
 
+    void increaseVelX(double val){vX+=val;}
+    double getVelX(){return vX;}
+    void increaseVelY(double val){vY+=val;}
+    double getVelY(){return vY;}
+
 };
 class Molecule{
 private:
     unsigned int count;
     Atom* atoms;
+
+    double cmX;
+    double cmY;
 
 public:
     Molecule(){
@@ -149,11 +167,13 @@ public:
         }
         atoms[count-1].setCharge(-charge);
 
+        calculateCenterMass();
+
 
     }
     int getAtomNumber(){return count;}
     Atom* getAtoms(){return atoms;}
-    void drawAtoms(unsigned int r, unsigned int res){
+    void drawAtoms(double r, unsigned int res){
         for(int i=0;i<count;i++){
             atoms[i].draw(r,res);
         }
@@ -163,6 +183,20 @@ public:
             atoms[i].drawLineToParent(res);
         }
     }
+    void calculateCenterMass(){
+        double mass=0;
+        cmX=0;
+        cmY=0;
+        for(int i=0;i<count;i++){
+            mass+=atoms[i].getMass();
+            //TODO handle small numbers, or change the Atom class;
+            cmX+=atoms[i].getMass()*atoms[i].getX();
+            cmY+=atoms[i].getMass()*atoms[i].getY();
+        }
+        cmX/=mass;
+        cmY/=mass;
+    }
+
 
     ~Molecule(){
         delete atoms;
@@ -184,7 +218,6 @@ void main(){
     vec4 trans=vec4(position,sqrt(mvPos.x*mvPos.x+mvPos.y*mvPos.y+1),1);
     gl_Position=vec4(trans.x/(trans.z+1),trans.y/(trans.z+1),0,1);
     outColor=color;
-
 }
 
 
@@ -203,9 +236,9 @@ void main(){
 void onInitialization() {
     glViewport(0, 0, windowWidth, windowHeight);
 
+
     glGenVertexArrays(1, &vertexArray);
     glBindVertexArray(vertexArray);
-    int t=offsetof(Vertex,col);
     glGenBuffers(1,&buffer);
     glBindBuffer(GL_ARRAY_BUFFER,buffer);
     glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,sizeof(Vertex),(void *)offsetof(Vertex, pos));
@@ -227,10 +260,11 @@ void onInitialization() {
     glAttachShader(program, fragmentShader);
 
 
-
     glLinkProgram(program);
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
+
+
     glutPostRedisplay();
 
 }
@@ -255,14 +289,12 @@ void onDisplay() {
             0, 0, 0, 1
     };
     glUniformMatrix4fv(mvpLocation,1,GL_FALSE,matrix);
-
-
     if(molecules.size()>0)
     {
 
         for (const auto &item : molecules){
             item->drawBonds(20);
-            item->drawAtoms(10,20);
+            item->drawAtoms(0.05f,20);
         }
     }
     glutSwapBuffers();
@@ -271,8 +303,30 @@ void onKeyboard(unsigned char key, int pX, int pY) {
     if(key==' '){
         molecules.push_back(new Molecule());
         molecules.push_back(new Molecule());
-        glutPostRedisplay();
+
     }
+    else if(key=='s' || key=='d' || key=='x' || key=='e'){
+        for (const auto &item : molecules){
+            for(int i=0;i<item->getAtomNumber();i++){
+                switch(key){
+                    case 's':
+                        item->getAtoms()[i].setX(item->getAtoms()[i].getX()-0.1);
+                        break;
+                    case 'd':
+                        item->getAtoms()[i].setX(item->getAtoms()[i].getX()+0.1);
+                        break;
+                    case 'x':
+                        item->getAtoms()[i].setY(item->getAtoms()[i].getY()-0.1);
+                        break;
+                    case 'e':
+                        item->getAtoms()[i].setY(item->getAtoms()[i].getY()+0.1);
+                        break;
+                }
+            }
+        }
+    }
+    glutPostRedisplay();
+
 
 
 }
@@ -291,8 +345,83 @@ void onMouse(int button, int state, int pX, int pY) {
     printf("\n%d, %d",pX,pY);
 }
 
+double calculateDistSq(double x1, double y1,double x2, double y2){
+    return (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
+}
+float calculateAngle(double x1, double y1, double x2, double y2){
+    return atan(fabs((y1-y2)/(x1-x2)))*180/M_PI;
+}
+
 
 void onIdle() {
+
+
+    if(molecules.size()>0) {
+        std::vector<Atom> allAtoms=std::vector<Atom>();
+
+        for(int m=0;m<molecules.size();m++){
+            for(int i=0;i<molecules[m]->getAtomNumber();i++){
+                allAtoms.push_back(molecules[m]->getAtoms()[i]);
+            }
+        }
+        double forcesX[allAtoms.size()];
+        double forcesY[allAtoms.size()];
+        printf("");
+
+        for (double t = 0; t < 0.01; t += 0.0001) {
+
+            //Coulomb
+            for (int i=0;i<allAtoms.size();i++){
+
+
+                for(int j=0;j<allAtoms.size();j++){
+                    if(j==i)
+                        continue;
+                    //calculate force between the two atoms
+                    double force = allAtoms[i].getCharge() * allAtoms[j].getCharge()*pow(10,-10)*1.60217663 * 8.988 /
+                            (calculateDistSq(allAtoms[i].getX(), allAtoms[i].getY(), allAtoms[j].getX(),
+                                                 allAtoms[j].getY()));
+
+                    //calculate the angle
+                    double a= calculateAngle(allAtoms[i].getX(), allAtoms[i].getY(), allAtoms[j].getX(),
+                                             allAtoms[j].getY());
+
+                    //calculate the x and y vectors of the force
+                    if(allAtoms[i].getX()<=allAtoms[j].getX())
+                        forcesX[i]=forcesX[i]+ cos(a)*force*(force>0 ? 1 :-1);
+                    else
+                        forcesX[i]=forcesX[i]+ cos(a)*force*(force>0 ? -1 :1);
+
+                    if(allAtoms[i].getY()<allAtoms[j].getY())
+                        forcesY[i]=forcesY[i]+ sin(a)*force*(force>0 ? 1 :-1);
+                    else
+                        forcesY[i]=forcesY[i]+ sin(a)*force*(force>0 ? -1 :1);
+
+                }
+                //apply drag
+                double mag=1; //magic variable
+                forcesX[i]-=0.47*mag*allAtoms[i].getVelX()*allAtoms[i].getVelX()/2;
+                forcesX[i]-=0.47*mag*allAtoms[i].getVelY()*allAtoms[i].getVelY()/2;
+
+                //change velocity
+                allAtoms[i].increaseVelX(forcesX[i]/allAtoms[i].getMass());
+                allAtoms[i].increaseVelY(forcesY[i]/allAtoms[i].getMass());
+
+            }
+            //Move the atoms
+            //TODO make it work... MVP?
+            for(int i=0;i<allAtoms.size();i++){
+                allAtoms[i].setX(allAtoms[i].getX()+allAtoms[i].getVelX());
+                allAtoms[i].setY(allAtoms[i].getY()+allAtoms[i].getVelY());
+            }
+
+        }
+
+
+
+
+    }
+
 }
 
 
